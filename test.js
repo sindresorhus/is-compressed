f4aa931cdc9c428e3bc94c6840cc79433796e495
Sindre Sorhus <sindresorhus@gmail.com>
2015-05-25 13:31:37 +0200

    minor tweaks

--- test.js
+++ test.js
@@ -5,6 +5,7 @@ var isCompressed = require('./');
 test(function (t) {
 	t.assert(isCompressed('unicorn.zip'));
 	t.assert(isCompressed('unicorn.7z'));
+	t.assert(isCompressed('unicorn.ZIP'));
 	t.assert(!isCompressed('unicorn.png'));
 	t.assert(!isCompressed('unicornzip'));
 	t.assert(!isCompressed('unicorn.txt'));
